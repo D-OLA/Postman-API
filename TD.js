a7eed7e37f41a254c8a1d4226f158e3f8109e477
David6299 <dheveed3@gmail.com>
2022-07-12 02:22:05 +0100

    updated?

--- TD.js
+++ TD.js
@@ -1,15 +1,60 @@
-let a = "john";
-let b = 23;
+let fn = "David";
+let mn = "O.G";
+let ln = "Olanrewaju"
 
-var k;
+var ability;
+const v = true;
 
-if (k>=23){
-    console.log("Good to go")
+if (ability>=23 && v===true){
+    console.log("Good to go");
+}else{
+    console.log("gettat");
 }
 
+ability = 29;
+
+if (ability>=23 || v===true){
+    console.log("Good to go");
+    return "Good to go";
+}else{
+    console.log("gettat");
+    return "gettat";
+}
+
+ability =12;
+
+if (ability>=23){
+    console.log("Good to go");
+}else{
+    console.log("gettat");
+}
+
+
+
+function sayIt(){
+    console.log("hi everyone");
+};
+sayIt();
+
+const shoutOut = "skrrrr";
+const shoutee = "tunde"
+
+var kud = function shoutIt(who){
+    console.log("skrrrrrrrrrrrrr!" + who);
+};
+kud("baby");
+
+function shr (shoutOut, shoutee){
+    return shoutOut + shoutee;
+};
+
 const e = [apple, mongoe, banana, soursop];
+console.log(typeof e); 
+console.log(e[2]);
 
-console.log(typeof e);
-
-K = 29;
+function getRandomDomain (domain){
+    console.log("your new Email is " + domain + "@gmail.com");
+}
+let exDomain = "dheveed345";
+getRandomDomain(exDomain);
 
